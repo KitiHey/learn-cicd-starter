7a9c1ffe858389221670fc2c33ef2dcdf1c0be94
KitiHey <tomoiagalaurentiu2017@gmail.com>
2025-08-25 17:48:48 +0200

    Solve security issues

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
@@ -88,9 +89,11 @@ func main() {
 	v1Router.Get("/healthz", handlerReadiness)
 
 	router.Mount("/v1", v1Router)
+	var duration time.Duration = 30 * time.Second
 	srv := &http.Server{
-		Addr:    ":" + port,
-		Handler: router,
+		Addr:              ":" + port,
+		Handler:           router,
+		ReadHeaderTimeout: duration,
 	}
 
 	log.Printf("Serving on port: %s\n", port)
